ab1662f939102d96638d94690afb5454500e9b3a
Mina Dawood <mina.latif@live.com>
2021-11-10 22:13:57 -0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -193,12 +193,6 @@
     },
   });
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000,
-  });
-
   // Testimonials carousel (uses the Owl Carousel library)
   $('.testimonials-carousel').owlCarousel({
     autoplay: true,
